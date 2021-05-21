90f27bdd16f633d8901a13db0c171c1fa0b1d97f
linabiel <linuka.b@gmail.com>
2021-05-21 17:02:51 +0100

    fetch not working

--- client/src/Containers/WeatherContainer.js
+++ client/src/Containers/WeatherContainer.js
@@ -10,28 +10,28 @@ const WeatherContainer = () => {
         getCities();
     }, [selectedCity])
 
-    const getCities = function () {
+    const getCities = async (selectedCity) => {
         const cityApi = `http://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=1a9a20046a26886e891582ce46507106`
         const forcastApi = `http://api.openweathermap.org/data/2.5/forecast?q=${selectedCity}&appid=a8f139819d7f5bf326630492c5f3a89a6`
         const city = {daily: null, forcast: null}
-        fetch(cityApi)
+
+
+        const cityInfo = await fetch(cityApi)
             .then(res => res.json())
-            .then(result => city.daily = result)
-            .then(
+            .then(result => city.daily=result)
+
+        const cityForcast = await 
                 fetch(forcastApi)
                     .then(res => res.json())
-                    .then(result1 => {
-                        console.log(result1)
-                        city.forcast = result1
-                        return setCities([...cities, city])
-                    })
-            )
-    }
+                    .then(result1 => city.forcast=result1 ) 
+                        
+        setCities([...cities, city])}
+                        
+    
 
     const onCitySubmit = function (city) {
         setSelectedCity(city);
     }
-
 
     return (
         <div>
