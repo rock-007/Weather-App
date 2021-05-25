47413ba8001fc725c2a367e39127ebd7cf6d7dd9
Wellington-Black <well1482@gmail.com>
2021-05-25 16:31:00 +0100

    Pushing develop branch

--- client/src/Containers/WeatherContainer.js
+++ client/src/Containers/WeatherContainer.js
@@ -8,30 +8,17 @@ import {
   deleteFavourite,
 } from "../services/FavouriteService";
 
-
 const WeatherContainer = () => {
-<<<<<<< HEAD
   const [cities, setCities] = useState([]);
   const [selectedCity, setSelectedCity] = useState(null);
   const [favourites, setFavourites] = useState(null);
   const [displayFavourites, setDisplayFavourites] = useState(null);
+
   useEffect(() => {
     if (selectedCity != null) {
       getCities(selectedCity);
     }
   }, [selectedCity]);
-=======
-    const [cities, setCities] = useState([]);
-    const [selectedCity, setSelectedCity] = useState(null);
-    const [favourites, setFavourites] = useState(null);
-    const [displayFavourites, setDisplayFavourites] = useState(null);
-
-    useEffect(() => {
-        if (selectedCity != null) {
-            getCities(selectedCity);
-        }
-    }, [selectedCity]);
->>>>>>> f3f46f8942e1f455529e83c9fa83ee04862ec39c
 
   useEffect(() => {
     getFavourite();
@@ -43,7 +30,6 @@ const WeatherContainer = () => {
     }
   }, [favourites]);
 
-<<<<<<< HEAD
   const getFavouriteCities = function (favourites) {
     console.log(favourites[0][0]["name"]);
     let favouiteFetch = favourites[0].map((eachCity) => {
@@ -129,73 +115,6 @@ const WeatherContainer = () => {
       {selectedCity != null ? <City cities={cities} /> : null}
     </div>
   );
-=======
-    const getFavouriteCities = function (favourites) {
-        let favouiteFetch = favourites[0].map((eachCity) => {
-            return fetch(
-                `http://api.openweathermap.org/data/2.5/weather?q=${eachCity["name"]}&appid=3031aac4ff517ddfc83b94a403d374b0`
-            );
-        });
-
-        Promise.all(favouiteFetch)
-            .then((res) => {
-                return Promise.all(
-                    res.map(function (res) {
-                        return res.json();
-                    })
-                );
-            })
-            .then((result) => setDisplayFavourites(result));
-    };
-
-    const getCities = function (selectedCity) {
-        const cityApi = `http://pro.openweathermap.org/data/2.5/forecast/hourly?q=${selectedCity}&appid=3031aac4ff517ddfc83b94a403d374b0`;
-        const forecastApi = `http://api.openweathermap.org/data/2.5/forecast?q=${selectedCity}&appid=1a9a20046a26886e891582ce46507106`;
-        Promise.all([fetch(cityApi), fetch(forecastApi)])
-            .then((res) => {
-                return Promise.all(
-                    res.map(function (res) {
-                        return res.json();
-                    })
-                );
-            })
-            .then((result) =>
-                setCities([{ daily: result[0], forecast: result[1] },
-                ])
-            );
-    };
-
-    const onCitySubmit = function (city) {
-        setSelectedCity(city);
-    };
-
-    const onClick = function (favourite) {
-        setFavourites([...favourites, favourite]);
-    };
-
-    const getFavourite = () => {
-        getFavourites().then((result) => setFavourites([result]));
-    };
-
-    const addFavourite = (favouriteCity) => {
-        postFavourite(favouriteCity).then(() => getFavourite());
-    };
-
-    let currentCity = {};
-
-    return (
-        <div>
-            <SearchForm
-                cities={cities}
-                onCitySubmit={onCitySubmit}
-                onClick={onClick}
-                addFavourite={addFavourite}
-            />
-            {displayFavourites != null ? (<DisplayFavouite displayFavourites1={displayFavourites} />) : null}
-            <City cities={cities} />
-        </div>
-    );
->>>>>>> f3f46f8942e1f455529e83c9fa83ee04862ec39c
 };
 
 export default WeatherContainer;
