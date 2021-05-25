43162e8309f28adae55a425409603b97c51382f1
Wellington-Black <well1482@gmail.com>
2021-05-25 16:54:00 +0100

    favourite working again

--- client/src/Components/DisplayFavouite.js
+++ client/src/Components/DisplayFavouite.js
@@ -26,3 +26,20 @@ const DisplayFavouite = ({ displayFavourites1, deleteFavourite2 }) => {
 };
 
 export default DisplayFavouite;
+
+// import UK from "../Components/UK.png";
+
+// const DisplayFavourite = ({ displayFavourites1 }) => {
+
+//     const display = displayFavourites1.map((eachFavourite) => {
+//         return <p>{eachFavourite["name"]}</p>;
+//     });
+//     return (
+//         <div>
+//             {display}
+//             <img id ="image-1"src={UK} alt="No image found" width="200"/>
+//         </div>
+//     );
+// };
+
+// export default DisplayFavourite;
--- client/src/Components/SearchForm.js
+++ client/src/Components/SearchForm.js
@@ -1,87 +1,31 @@
-<<<<<<< HEAD
 import "./SearchForm.css";
 import "./NavBar.css";
 import { useState } from "react";
 import { postFavourite } from "../services/FavouriteService";
 import { addFavourite } from "../Containers/WeatherContainer";
+import CCBC from "../Components/CCBC.png";
 const SearchForm = ({ onCitySubmit, addFavourite, onClick }) => {
   const [search, setSearch] = useState();
-  // const onSubmit = (e) => {
-  //     e.preventDefault();
-  //     postBooking(favourite).then((data) => {
-  //         addBooking(data);
-  //     })
-
-  // const handleSubmit = function (event) {
-  //     event.preventDefault();
-  //     const chosenCity = event.target.city.value;
-  //     // const chosenCity = "Edinburgh";
-  //     onCitySubmit(chosenCity);
-  // };
 
   const handleSubmit = function (event) {
     event.preventDefault();
     if (search === 1) {
       // search
       const chosenCity = event.target.city.value;
-      // const chosenCity = "Edinburgh";
       onCitySubmit(chosenCity);
     } else if (search === 2) {
       // save in DB
       const favourite = event.target.city.value;
       addFavourite(favourite);
     }
-=======
-import './SearchForm.css';
-import './NavBar.css';
-import {useState} from "react";
-import {postFavourite} from "../services/FavouriteService";
-import {addFavourite} from "../Containers/WeatherContainer";
-import CCBC from "../Components/CCBC.png";
-const SearchForm = ({onCitySubmit, addFavourite, onClick}) => {
-const [search, setSearch]= useState()
-
-    const handleSubmit = function (event) {
-        event.preventDefault();
-    if (search === 1){
-    // search
-    const chosenCity = event.target.city.value;
-    onCitySubmit(chosenCity);
-      
-
-    } else if (search === 2){
-    // save in DB
-    const favourite = event.target.city.value;
-    addFavourite(favourite);
-    };       
-    
-    }
-    return (
-        <div id="form-wrapper" className="nav-bar">
-            <div id="header-left">
-                <img id ="image-2"src={CCBC} alt="No image found" width="200"/>
-                <h1 id="header">Weather</h1>
-            </div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    name="city"
-                    id="city"
-                    placeholder="City"
-                />
-                <button id="search-button" type="submit" onClick={()=>{setSearch(1)}}>Search</button>
-                <input onClick={()=>{setSearch(2)}} type="submit" value="Add to Favourites" id="save" />
-            </form>
-        </div>
-    );
-};
->>>>>>> f3f46f8942e1f455529e83c9fa83ee04862ec39c
-
     event.target.reset();
   };
   return (
     <div id="form-wrapper" className="nav-bar">
-      <h1 id="header">Weather</h1>
+      <div id="header-left">
+        <img id="image-2" src={CCBC} alt="No image found" width="200" />
+        <h1 id="header">Weather</h1>
+      </div>
       <form onSubmit={handleSubmit}>
         <input type="text" name="city" id="city" placeholder="City" />
         <button
@@ -105,5 +49,32 @@ const [search, setSearch]= useState()
     </div>
   );
 };
+
+// return (
+//   <div id="form-wrapper" className="nav-bar">
+//     <h1 id="header">Weather</h1>
+//     <form onSubmit={handleSubmit}>
+//       <input type="text" name="city" id="city" placeholder="City" />
+//       <button
+//         id="search-button"
+//         type="submit"
+//         onClick={() => {
+//           setSearch(1);
+//         }}
+//       >
+//         Search
+//       </button>
+//       <input
+//         onClick={() => {
+//           setSearch(2);
+//         }}
+//         type="submit"
+//         value="Add to Favourites"
+//         id="save"
+//       />
+//     </form>
+//   </div>
+// );
+// };
 
 export default SearchForm;
