8d5d764ebee619713fae6f612db1cd2ddab5c10b
rock-007 <umair.ashraf2019@gmail.com>
2021-05-27 19:53:08 +0100

    config mongo collection

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
     mongoURI:
-        "mongodb+srv://umair:Pakistan008%@cluster0.untiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        "mongodb+srv://umair:skyliner@cluster0.untiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 };
--- server.js
+++ server.js
@@ -6,29 +6,35 @@ app.use(express.json());
 // const cors = require("cors");
 // app.use(cors());
 
-const MongoClient = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
 const createRouter = require("./helpers/create_router.js");
 
 const mongoCloudURI = require("./config/keys").mongoURI;
 //MongoClient.connect("mongodb://localhost:27017")
-
+console.log("xx", mongoCloudURI);
 // const client = new MongoClient(mongoCloudURI, {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true,
 // });
 
-MongoClient.connect(mongoCloudURI, function (err, client) {
-    if (err) {
-        console.log("Unable to connect to the mongoDB server. Error:", err);
-    } else {
-        console.log("Connection established to", url);
-        db = client.db("weather");
-        const favouritesCollection = db.collection("favourites");
-        const favouritesRouter = createRouter(favouritesCollection);
-        console.log("xxxxx", db, favouritesCollection, favouritesRouter);
-        app.use("/api/favourites", favouritesRouter);
+mongoose.connect(
+    "mongodb+srv://umair:skyliner@cluster0.untiw.mongodb.net/weather?retryWrites=true&w=majority",
+    function (err, client) {
+        if (err) {
+            console.log("Unable to connect to the mongoDB server. Error:", err);
+        } else {
+            console.log("Connection established to", client);
+            let collection = client.db;
+            let favourites = collection.collection("favourites");
+            // const favouritesCollection = db.collection("favourites");
+            const favouritesRouter = createRouter(favourites);
+            console.log("xxxx11x", favouritesRouter);
+            console.log("ccx", favourites);
+
+            app.use("/api/favourites", favouritesRouter);
+        }
     }
-});
+);
 // const MongoClient = require('mongodb').MongoClient;
 //     const uri = "mongodb+srv://umair:Pakistan008@@cluster0.untiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 //     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
