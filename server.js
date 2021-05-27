a18be3a0f96e039d0fe38cf9e09d773cae5f1ba1
rock-007 <umair.ashraf2019@gmail.com>
2021-05-27 23:28:47 +0100

    d

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ mongoose.connect(
             console.log("xxxx11x", favouritesRouter);
             console.log("ccx", favourites);
 
-            app.use("/api/favourites", favouritesRouter);
+            app.use("/api/favourites/", favouritesRouter);
         }
     }
 );
